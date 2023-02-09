f322df5f1d3c1a5082e61021c1da079fceb0d9a3
aajardi022 <aajardi022@student.wethinkcode.co.za>
2023-02-09 14:01:14 +0200

    new

---
+++ OTHER/range.py
@@ -0,0 +1,9 @@
+"""generates the immutable sequence of numbers from the start
+integer to the stop integer
+you can iterate with a for loop"""
+
+# Generate numbers between 0 to 6
+for i in range(6): # never includes the stop number in its result.
+    print(i)
+
+# range(start, stop[, step])
