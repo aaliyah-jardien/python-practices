30c0f169491622d06a3cbbe87b6c677883383aef
aajardi022 <aajardi022@student.wethinkcode.co.za>
2023-02-09 14:08:57 +0200

    indexing

---
+++ OTHER/list-index.py
@@ -0,0 +1,17 @@
+"""
+list indexing
+
+searches the given element from the start of the list
+and returns the index of the first occurance
+
+Syntax: list_name.index(element, start, end) 
+
+useful (searching) shows which position something is
+
+https://www.geeksforgeeks.org/python-list-index/
+"""
+# list of items
+list2 = ['cat', 'bat', 'mat', 'cat', 'pet']
+ 
+# Will print the index of 'bat' in list2
+print(list2.index('bat'))
