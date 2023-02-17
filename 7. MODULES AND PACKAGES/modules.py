f43ae6de3b1066698c65659d56251d9c62059220
aajardi022 <aajardi022@student.wethinkcode.co.za>
2023-02-17 16:32:05 +0200

    new modules

---
+++ 7. MODULES AND PACKAGES/modules.py
@@ -0,0 +1,29 @@
+# MODULES AND PACKAGES
+"""
+In this topic we will:
+
+    learn what is modular programming.
+
+    be introduced to the programming concept of namespaces.
+
+    see how we can use packages to provide a hierarchical structure to our modules
+
+    learn how to use other modules (or libraries) in our code.
+"""
+#  STEP 1 Define a Module
+# Type the following code in a file called my_module.py, and save it:
+my_string = "I am inevitable."
+lucky_number = 21
+
+def do_something():
+    print('Tell me why, and I tell you why not.')
+
+"""
+This actually defines a module my_module, with the following elements in it:
+a string my_string
+
+a number lucky_number
+
+a function do_something()
+
+"""
